06e29451675eb9f423829f41c300ff5c9c42a9ed
John Boerchers <johnnyboerchers@gmail.com>
2022-12-12 18:41:15 -0500

    try

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_style = "/"
-html_theme_path = ['.']
-html_sidebars = {}
-html_theme_config = []
 html_static_path = ['_static']
