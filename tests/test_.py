275772ac3c2b10fd9b4098010950a1b1aa5fa2b8
John Boerchers <johnnyboerchers@gmail.com>
2022-12-14 09:28:41 -0500

    Plotter now makes output/plots dir if it doesn't exist

--- tests/test_.py
+++ tests/test_.py
@@ -1,11 +1,13 @@
 import sys
 import numpy as np
+import os
 
 sys.path.append("..")
 
 from src.input import PsychoInput
 from src.mesh import PsychoArray
 from src.pgen.sample import sampleProblemGenerator
+from plotting.plotter import Plotter
 
 
 def test_psycho_input():
@@ -35,3 +37,28 @@ def test_psycho_pgen():
     assert np.all(pmesh.Un[0, :, :] == 1.0)
     assert np.all(pmesh.Un[1, :, :] == 0.0)
     assert np.all(pmesh.Un[2, :, :] == 0.0)
+
+
+def test_plotter_directory():
+
+    # Remove directory if it exists to test the 
+    # plotter's ability to create it
+    if os.path.exists("./output/plots"):
+        os.removedirs("./outputs/plots")
+
+    assert not os.path.exists("./output/plots")
+
+    pin = PsychoInput(f"inputs/sample.in")
+
+    pin.parse_input_file()
+
+    pmesh = PsychoArray(pin, np.float64)
+
+    sampleProblemGenerator(pin=pin, pmesh=pmesh)
+
+    test_plotter = Plotter(pmesh)
+    test_plotter.check_path_exists()
+
+    assert os.path.exists("./output/plots")
+
+    
