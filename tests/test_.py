8a7ef37f2e44ebe1a18ac27d8ede3763a2b8f4b2
Sydney Rzepka <sr5286@princeton.edu>
2022-12-15 08:51:24 -0500

    adding more tests

--- tests/test_.py
+++ tests/test_.py
@@ -1,6 +1,5 @@
 import sys
 import numpy as np
-import math
 
 sys.path.append("..")
 
@@ -8,12 +7,10 @@ from src.input import PsychoInput
 from src.mesh import PsychoArray, get_interm_array
 from src.pgen.kh import ProblemGenerator
 from src.eos import p_EOS,e_EOS
-from src.reconstruct import get_limited_slopes,get_unlimited_slopes
-from src.riemann import solve_riemann
-from src.tools import get_primitive_variables_1d, get_primitive_variables_2d, get_fluxes_1d, get_fluxes_2d
+from src.reconstruct import get_limited_slopes
+from src.tools import get_primitive_variables_1d, get_primitive_variables_2d, get_fluxes_1d, get_fluxes_2d, calculate_timestep
 from src.data_saver import PsychoOutput
 from plotting.plotter import Plotter
-
 
 
 
@@ -25,13 +22,17 @@ def test_psycho_input():
 
     pin.parse_input_file()
 
-    assert pin.value_dict["rho0"] == 1.0
     assert pin.value_dict["rho1"] > pin.value_dict["rho0"]
+    assert pin.value_dict["rho0"] > 0.0
+    assert pin.value_dict["rho1"] > 0.0
     assert pin.value_dict["p0"] == pin.value_dict["p1"]
-    assert pin.value_dict["u0"] >= 0
-    assert pin.value_dict["u1"] < 0
+    assert pin.value_dict["p0"] > 0.0
+    assert pin.value_dict["u0"] >= 0.0
+    assert pin.value_dict["u1"] < 0.0
     assert pin.value_dict["pert_amp"] < 1.0
+    assert pin.value_dict["pert_amp"] > 0.0
     assert pin.value_dict["CFL"] < 1.0
+    assert pin.value_dict["CFL"] > 0.0
     assert pin.value_dict["gamma"] == 1.4
     assert pin.value_dict["left_bc"] == "periodic"
     assert pin.value_dict["right_bc"] == "periodic"
@@ -59,14 +60,14 @@ def test_psycho_pgen():
     assert pmesh.Un[1, :, :] ==  0.3 # rho0*u0
     assert pmesh.Un[1, :, np.abs(y) >= 0.25] == -0.6 # rho1 * u1
 
-    
-    # not checking rho*v because v is random
-    # not checking pressures array because it is just set to p0 which is checked in input test
+
+    nx1 = pin.value_dict["nx1"]
+    nx2 = pin.value_dict["nx2"]
+    ng = pin.value_dict["ng"]
+    assert pmesh.Un.ndim == (nx1,nx2)
     
     assert pmesh.Un[3, :, :] >= 0 # total energy is positive
 
-#def test_psycho_output():
-# check dimensions, whether files exist, etc
 
 def test_psycho_eos():
     """Tests eos.py by generating random inputs and compares the output of eos.py with a direct application of the equation of state equation
@@ -91,8 +92,10 @@ def test_psycho_eos():
 
     gamma = 1.4
     pstate = p_EOS(rho,e,gamma)
+    assert pstate[:][:] > 0.0
 
     estate = e_EOS(rho,p,gamma)
+    assert estate[:][:] > 0.0
 
     tol = 0.001
     count = 0.0
@@ -116,8 +119,22 @@ def test_psycho_mesh():
     """
     pin = PsychoInput(f"inputs/kh.in")
     pmesh = PsychoArray(pin, np.float64)
-    # check array dimensions
-    # check that boundary conditions are enforced correctly
+    
+    assert pmesh.Un.ndim == (pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    
+    interm = np.array()
+    interm = pmesh.get_interm_array(pmesh.nvar, pmesh.nx1, pmesh.nx2)
+    if (pmesh.nvar==1):
+        assert interm.ndim == (pmesh.nx1, pmesh.nx2)
+    else:
+        assert interm.ndim == (pmesh.nvar, pmesh.nx1, pmesh.nx2)
+
+
+def test_bc_enforced():
+    """Check boundary condition enforcement
+
+    """
+
 
 def test_psycho_reconstruct():
     """Since reconstruct.py essentially finds a linear interpolation between values at the cell faces, the slope of the line that it finds should have a magnitude of 1 or smaller since our grid is made of squares. This test checks for that.
@@ -150,117 +167,256 @@ def test_psycho_reconstruct():
     assert abs(delta_j) <= 1.0
 
 
-def test_psycho_tools():
-    """Tests that array dimensions in tools.py are correct. 
+def test_psycho_1d_variables():
+    """Tests that array dimensions in get_primitive_variables_1d() in tools.py are correct. 
 
     """
-    get_primitive_variables_1d()
+    pin = PsychoInput(f"inputs/kh.in")
+    pmesh = PsychoArray(pin, np.float64)
 
-    get_primitive_variables_2d()
+    pin.parse_input_file()
 
-    get_fluxes_1d()
+    gamma = pin.value_dict["gamma"]
 
-    get_fluxes_2d()
+    rho, u, v, p = get_primitive_variables_1d(pmesh.Un, gamma)
+
+    assert rho.ndim == (4)
+    assert rho[:] > 0.0
+    assert u.ndim == (4)
+    assert v.ndim == (4)
+    assert p.ndim == (4)
+    assert p[:] > 0.0
+
+
+def test_psycho_2d_variables():
+    """Tests that array dimensions in get_primitive_variables_2d() in tools.py are correct. 
+
+    """
+    pin = PsychoInput(f"inputs/kh.in")
+    pmesh = PsychoArray(pin, np.float64)
+
+    pin.parse_input_file()
+
+    gamma = pin.value_dict["gamma"]
+
+    rho, u, v, p = get_primitive_variables_2d(pmesh.Un, gamma)
+
+    assert rho.ndim == (pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    assert rho[:,:] > 0.0
+    assert u.ndim == (pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    assert v.ndim == (pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    assert p.ndim == (pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    assert p[:,:] > 0.0
+
+
+
+def test_psycho_1d_fluxes():
+    """Tests that array dimensions in get_fluxes_1d() in tools.py are correct. 
+
+    """
+    pin = PsychoInput(f"inputs/kh.in")
+    pmesh = PsychoArray(pin, np.float64)
+
+    pin.parse_input_file()
+
+    gamma = pin.value_dict["gamma"]
+
+    Fx = get_fluxes_1d(pmesh.Un, gamma, 'x')
+    assert Fx.ndim == (4)
+    assert Fx[1] >= 0.0
+
+    Fy = get_fluxes_1d(pmesh.Un, gamma, 'y')
+    assert Fy.ndim == (4)
+    assert Fy[2] >= 0.0
+
+
+
+def test_psycho_2d_fluxes():
+    """Tests that array dimensions in get_fluxes_2d() in tools.py are correct. 
+
+    """
+
+    pin = PsychoInput(f"inputs/kh.in")
+    pmesh = PsychoArray(pin, np.float64)
+
+    pin.parse_input_file()
+
+    gamma = pin.value_dict["gamma"]
+
+
+    Fx = get_fluxes_2d(pmesh.Un, gamma, 'x')
+    assert Fx.ndim ==(4, pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    assert Fx[1, :, :] >= 0.0
+
+    Fy = get_fluxes_2d(pmesh.Un, gamma, 'y')
+    assert Fy.ndim ==(4, pmesh.nvar, pmesh.nx1 + 2 * pmesh.ng, pmesh.nx2 + 2 * pmesh.ng)
+    assert Fy[1, :, :] >= 0.0
+
+
+def test_calculate_timestep():
+    """Tests that array dimensions in calculate_timestep() in tools.py are correct. 
+
+    """
+    pin = PsychoInput(f"inputs/kh.in")
+    pmesh = PsychoArray(pin, np.float64)
+
+    pin.parse_input_file()
+
+    cfl = float(pin.value_dict["CFL"])
+    gamma = float(pin.value_dict["gamma"])
+
+    assert calculate_timestep(pmesh, cfl, gamma) > 0.0
+
+
+
+
+def test_psycho_data_file_existence():
+    """Tests that correct data files exist.
+    array dimensions in psycho_data_saver.py are correct. 
+
+    """
+    pin = PsychoInput(f"inputs/kh.in")
+    pin.parse_input_file()
+
+    pout = PsychoOutput(input_fname=input_fname)
+    pout.data_preferences(pin)
+
+
+    if "txt" in pout.file_type:
+        if "x-velocity" in pout.variables:
+
+            assert os.path.isfile('x-velocity.txt')
+
+        if "y-velocity" in pout.variables:
+
+            assert os.path.isfile('y-velocity.txt')
+
+        if "density" in pout.variables:
+
+            assert os.path.isfile('density.txt')
+
+        if "pressure" in pout.variables:
+
+            assert os.path.isfile('pressure.txt')
+
+
+    if "csv" in pout.file_type:
+        if "x-velocity" in pout.variables:
+
+            assert os.path.isfile('x-velocity.csv')
+
+        if "y-velocity" in pout.variables:
+
+            assert os.path.isfile('y-velocity.csv')
+
+        if "density" in pout.variables:
+
+            assert os.path.isfile('density.csv')
+
+        if "pressure" in pout.variables:
+
+            assert os.path.isfile('pressure.csv')
+
+    if "hdf5" in pout.file_type:
+        
+         assert os.path.isfile(f'iter_{file_name}.hdf5')
+
+         if "x-velocity" in pout.variables:
+
+            assert 
+
+        if "y-velocity" in pout.variables:
+
+            assert 
+
+        if "density" in pout.variables:
+
+            assert 
+
+        if "pressure" in pout.variables:
+
+            assert 
+
+
+def test_psycho_data_saved_to_file():
+    """Tests that array dimensions in psycho_data_saver.py are correct. 
+
+    """
+    pin = PsychoInput(f"inputs/kh.in")
+    pin.parse_input_file()
+
+    pmesh = PsychoArray(pin, np.float64)
+
+    ProblemGenerator(pin=pin, pmesh=pmesh)
+
+    pout = PsychoOutput(input_fname=input_fname)
+    pout.data_preferences(pin)
+
+    gamma = pin.value_dict["gamma"]
+
+    rho, u, v, p = get_primitive_variables_2d(pmesh,gamma)
+
+    
+    if "txt" in pout.file_type:
+        if "x-velocity" in pout.variables:
+
+            assert 
+
+        if "y-velocity" in pout.variables:
+
+            assert 
+
+        if "density" in pout.variables:
+
+            assert 
+
+        if "pressure" in pout.variables:
+
+            assert 
+
+
+    if "csv" in pout.file_type:
+        if "x-velocity" in pout.variables:
+
+            assert 
+
+        if "y-velocity" in pout.variables:
+
+            assert 
+
+        if "density" in pout.variables:
+
+            assert 
+
+        if "pressure" in pout.variables:
+
+            assert 
+
+    if "hdf5" in pout.file_type:
+        
+         assert os.path.isfile(f'iter_{file_name}.hdf5')
+
+         if "x-velocity" in pout.variables:
+
+            assert 
+
+        if "y-velocity" in pout.variables:
+
+            assert 
+
+        if "density" in pout.variables:
+
+            assert 
+
+        if "pressure" in pout.variables:
+
+            assert 
+
+
 
 
 
 #def test_psycho_plotting():
 # check array dimensions
 # not sure what other tests can be done for plotter...
-
-#def test_psycho_primary_conservation():
-
-    #get_primitive_variables_1d()
-
-    #get_primitive_variables_2d()
-
-    #get_fluxes_1d()
-
-    #get_fluxes_2d()
-
-    #assert 
-
-#def test_psycho_secondary_conservation():
-
-
-# BEFORE RUNNING CONVERGENCE TESTS, kh.in FILE OUTPUT NEEDS TO BE TXT
-def test_psycho_temporal_convergence():
-    """Tests for temporal convergence with error of order 1 or greater. It should execute psycho.py with different 512 cells in each direction. Then it should follow basically what we do in 557. This one and the next really need to be tested
-
-    """
-
-    os.system('python psycho.py -p temp_convergence')
-
-    pin = PsychoInput(f"inputs/kh.in")
-    pmesh = PsychoArray(pin, np.float64)
-
-    pin.parse_input_file()
-
-    ng = pin.value_dict["ng"]
-    
-    u_reg_dx = np.loadtxt('x-velocity.txt', dtype=float)
-    u_small_dx = pmesh.Un[1, ng : -ng, ng : -ng] / rho
-
-    v_reg_dy = np.loadtxt('y-velocity.txt', dtype=float)
-    v_small_dy = pmesh.Un[2, ng : -ng, ng : -ng] / rho
-
-    x = np.linspace(pin.value_dict["x1min"],pin.value_dict["x1max"],pin.value_dict["nx1"] + 2 * pin.value_dict["ng"])
-    dx = (abs(pin.value_dict["x1max"]) + abs(pin.value_dict["x1min"]))/range(x)
-    dy = dx
-    count = 0
-
-    for j in range(ng):
-        for i in range(ng):
-            u_err[i][j] = abs(u_reg_dx[i][j] - u_small_dx[i][j])
-            v_err[i][j] = abs(v_reg_dy[i][j] - v_small_dy[i][j])
-
-            n_u[i][j] = math.log(u_err[i][j],10) / math.log(dx,10)
-            n_v[i][j] = math.log(v_err[i][j],10) / math.log(dy,10)
-
-            if (n_u[i][j]>=1) and (n_v[i][j>=1]):
-                count = count +1
-
-    assert count >= 0.95*ng*ng
-
-
-def test_psycho_spatial_convergence():
-    """Tests for spatial convergence with error of order 1 or greater. It should execute psycho.py with a CFL of 0.25 rather than 0.5. Then it should follow basically what we do in 557.
-
-    """
-
-    os.system('python psycho.py -p spatial_convergence') 
-
-    pin = PsychoInput(f"inputs/kh.in")
-    pmesh = PsychoArray(pin, np.float64)
-
-    pin.parse_input_file()
-
-    ng = pin.value_dict["ng"]
-
-    u_reg_dt = np.loadtxt('x-velocity.txt', dtype=float)
-    u_small_dt = pmesh.Un[1, ng : -ng, ng : -ng] / rho
-
-    v_reg_dt = np.loadtxt('y-velocity.txt', dtype=float)
-    v_small_dt = pmesh.Un[2, ng : -ng, ng : -ng] / rho
-
-    x = np.linspace(pin.value_dict["x1min"],pin.value_dict["x1max"],pin.value_dict["nx1"] + 2 * pin.value_dict["ng"])
-    dx = (abs(pin.value_dict["x1max"]) + abs(pin.value_dict["x1min"]))/range(x)
-    dy = dx
-    count = 0
-
-    for j in range(ng):
-        for i in range(ng):
-            u_err[i][j] = abs(u_reg_dt[i][j] - u_small_dt[i][j])
-            v_err[i][j] = abs(v_reg_dt[i][j] - v_small_dt[i][j])
-
-            n_u[i][j] = math.log(u_err[i][j],10) / math.log(dx,10)
-            n_v[i][j] = math.log(v_err[i][j],10) / math.log(dy,10)
-
-            if (n_u[i][j]>=1) and (n_v[i][j>=1]):
-                count = count +1
-
-    assert count >= 0.95*ng*ng
-
-
-
 
